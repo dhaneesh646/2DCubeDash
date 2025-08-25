8b1a5e3a1d2247c36d66a01c3ec8460b1f514227
dhaneesh646 <85044565+dhaneesh646@users.noreply.github.com>
2025-08-25 12:52:12 +0530

    home page added

---
+++ Assets/ProjectResources/Scripts/Common/HomePageMaskController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class HomeageMaskController : MonoBehaviour
+{
+    [SerializeField] Transform maskTransform; // assign the SpriteMask
+    Camera mainCamera;
+
+    void Awake()
+    {
+        mainCamera = Camera.main;
+    }
+
+    void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = Mathf.Abs(mainCamera.transform.position.z); // distance from camera
+        Vector3 worldPos = mainCamera.ScreenToWorldPoint(mousePos);
+
+        maskTransform.position = new Vector3(worldPos.x, worldPos.y, maskTransform.position.z);
+    }
+}
